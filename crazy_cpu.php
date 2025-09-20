210b2bc5bdcee2f77a93d67a478a0d87e90110af
acardosojmz <cardosojmz@gmail.com>
2025-09-20 08:14:54 -0600

    se agregaron archivos de prueba

---
+++ crazy_cpu.php
@@ -0,0 +1,1 @@
+<?php for ($i=0; $i<1000000; $i++){ $x = $i*$i; } echo "CPU Test Complete"; ?>
---
+++ get_server.php
@@ -0,0 +1,4 @@
+<?php
+// Muestra el nombre del host que atiende la solicitud
+echo "Bienvenido lo atiende su majestad: " . gethostname();
+?>
